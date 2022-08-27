df6488f92227f6adc90f50659a935f4499145a96
Adrian Moore <adrianrob@hotmail.com>
2022-08-27 11:59:26 -0700

    Add GUI option to rotate model

--- src/examples/spriograph/spirograph.component.ts
+++ src/examples/spriograph/spirograph.component.ts
@@ -1,3 +1,4 @@
+import { make, NgtTriple } from "@angular-three/core";
 import { AfterViewInit, Component, OnInit } from "@angular/core";
 import GUI from "lil-gui";
 
@@ -9,6 +10,7 @@ import { CameraService } from "../../app/camera.service";
   templateUrl: './spirograph.component.html'
 })
 export class SpirographExample implements OnInit, AfterViewInit {
+  position = [0, 2, -1] as NgtTriple;
   arm1!: Object3D;
   arm2!: Object3D;
 
@@ -19,14 +21,15 @@ export class SpirographExample implements OnInit, AfterViewInit {
   constructor(
     private cameraService: CameraService,
   ) {
-    this.cameraService.position = [0, 1.5, 1];
-    this.cameraService.lookAt = [0, 1.5, 0];
-    this.cameraService.fov = 45;
+    this.cameraService.position = [0, 1.7, 1];
+    this.cameraService.lookAt = [0, 1.7, 0];
+    this.cameraService.fov = 65;
   }
 
   public parameters = {
-    changex: true,
-    changez: true,
+    changex: 1,
+    changez: 1,
+    animate: 0,
     arm1length: 0.5,
     arm2length: 0.1,
     arm2factorx: 30,
@@ -35,18 +38,21 @@ export class SpirographExample implements OnInit, AfterViewInit {
   };
 
   public gui!: GUI;
+  private origin!: Vector3;
 
   ngOnInit(): void {
     const gui = new GUI({ width: 300 }).title('Draw Settings');
     gui.add(this.parameters, 'arm1length', 0.1, 1.0, 0.1).name('Arm 1 Length');
     gui.add(this.parameters, 'arm2length', 0.03, 0.4, 0.001).name('Arm 2 Length');
-    gui.add(this.parameters, 'changex').name('Allow change along X');
+    gui.add(this.parameters, 'changex', 0, 1, 1).name('Allow change along X');
     gui.add(this.parameters, 'arm2factorx', 0, 90, 1).name('Rotations along X');
-    gui.add(this.parameters, 'changez').name('Allow change along Z');
+    gui.add(this.parameters, 'changez', 0, 1, 1).name('Allow change along Z');
     gui.add(this.parameters, 'arm2factorz', 0, 90, 1).name('Rotations along Z');
+    gui.add(this.parameters, 'animate', 0, 1, 1).name('Rotation Preview');
     gui.add(this.parameters, 'redraw').name('Redraw');
     this.gui = gui;
 
+    this.origin = make(Vector3, this.position);
   }
 
   ngAfterViewInit(): void {
@@ -73,16 +79,16 @@ export class SpirographExample implements OnInit, AfterViewInit {
         if (this.angle <= 360) {
 
           this.arm1.rotation.z = -MathUtils.degToRad(this.angle);
-          if (this.parameters.changex) {
+          if (this.parameters.changex == 1) {
             this.arm2.rotation.x = -MathUtils.degToRad(this.angle) * this.parameters.arm2factorx;
           }
 
-          if (this.parameters.changez) {
+          if (this.parameters.changez == 1) {
             this.arm2.rotation.z = -MathUtils.degToRad(this.angle) * this.parameters.arm2factorz;
           }
           const next = new Vector3();
-          next.setFromMatrixPosition(tip.matrixWorld);
-          if (this.angle < 360) this.vectors.push(next)
+          const position = tip.getWorldPosition(next).sub(this.origin)
+          if (this.angle < 360) this.vectors.push(position)
 
           this.arm1.updateMatrixWorld();
 
@@ -101,5 +107,11 @@ export class SpirographExample implements OnInit, AfterViewInit {
         this.enabled = false;
       }
     }
+    if (this.parameters.animate) {
+      this.tubemesh.rotation.x = 0;
+      this.tubemesh.rotation.y += 0.005;
+      this.tubemesh.rotation.z = 0;
+    }
   }
+  
 }
