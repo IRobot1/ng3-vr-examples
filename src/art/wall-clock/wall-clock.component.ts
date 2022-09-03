bd8e76b0a93e35ee9813a350de400ec6e2c01c87
Adrian Moore <adrianrob@hotmail.com>
2022-09-03 15:54:35 -0700

    Customization of wall clock added

--- src/art/wall-clock/wall-clock.component.ts
+++ src/art/wall-clock/wall-clock.component.ts
@@ -1,55 +1,72 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 import { Observable, Subscription, timer } from "rxjs";
 
 import { MathUtils } from "three";
 import { NgtTriple } from "@angular-three/core";
 
-class TickData {
-  constructor(public position: NgtTriple, public angle: number) { }
-}
 
 @Component({
   selector: 'wall-clock',
   templateUrl: './wall-clock.component.html',
 })
-export class WallClockComponent {
+export class WallClockComponent implements OnInit {
   @Input() position = [0, 0, 0] as NgtTriple;
   @Input() rotation = [0, 0, 0] as NgtTriple;
   @Input() scale = [1, 1, 1] as NgtTriple;
 
-  ticks: Array<TickData> = []
+  @Input() showrim = true;
+  @Input() showsecondticks = true;
+  @Input() shownumbers = true;
 
-  hour= 0
-  minute=0
-  second=0
+  @Input() color = 'black';
+  @Input() rimcolor = 'black';
+  @Input() secondcolor = 'red';
+  @Input() numbercolor = 'black';
 
-  private timer: Observable<number> = timer(0,1000);
+  ticks: Array<number> = []
+  secondticks: Array<number> = []
+
+  hour = 0
+  minute = 0
+  second = 0
+
+  private timer: Observable<number> = timer(0, 1000);
   private subs = new Subscription();
 
-  constructor() {
-    const PI2 = Math.PI * 2
 
+  ngOnInit(): void {
     const sides = 12
-    const rotation = MathUtils.degToRad(360 / sides);
+    let rotation = MathUtils.degToRad(360 / sides);
 
     for (let i = 0; i < sides; i++) {
-      const angle = (i / sides) * PI2
-      this.ticks.push(new TickData([Math.sin(angle), Math.cos(angle), 0], rotation * i));
+      this.ticks.push(rotation * i);
+    }
+
+    if (this.showsecondticks) {
+      const minutes = 60;
+      rotation = MathUtils.degToRad(360 / minutes);
+      for (let i = 0; i < minutes; i++) {
+        if (i % 5 > 0)
+          this.secondticks.push(rotation * i);
+      }
     }
 
     this.subs.add(this.timer.subscribe(() => {
       const now = new Date();
 
-      const hour = now.getHours();
-      this.hour= -MathUtils.degToRad(hour * 360 / 12);
-
       const minute = now.getMinutes();
-      this.minute=-MathUtils.degToRad(minute * 360 / 60);
+      this.minute = -MathUtils.degToRad(minute * 360 / 60);
+
+      const hour = now.getHours();
+      const hourangle = 360 / 12;
+      const subhourangle = hourangle / 60;
+      this.hour = -MathUtils.degToRad(hour * hourangle) - MathUtils.degToRad(minute * subhourangle);
 
       const second = now.getSeconds();
-      this.second=-MathUtils.degToRad(second * 360 / 60);
-  
+      this.second = -MathUtils.degToRad(second * 360 / 60);
+
     }));
+
   }
 }
