cf4bc246a53a9d6a2d1f7c567bfa40b08cbaf8ed
Adrian Moore <adrianrob@hotmail.com>
2022-09-04 18:54:14 -0700

    Gap between loading to improve VR experience

--- src/examples/teleport/teleport.component.ts
+++ src/examples/teleport/teleport.component.ts
@@ -9,9 +9,17 @@ export class TeleportExample {
   trackpointer = true;
   teleport = true;
 
+  stage = new Array(9).fill(false);
+
   constructor(private cameraService: CameraService) {
     this.cameraService.position = [0, 1.5, 1];
 
+    let stage = 0;
+    const timer = setInterval(() => {
+      this.stage[stage] = true;
+      stage++;
+      if (stage > 8) clearInterval(timer);
+    }, 1250)
     // uncomment to test enable/disable at runtime
     //setInterval(() => { this.teleport = !this.teleport }, 5000);
   }
