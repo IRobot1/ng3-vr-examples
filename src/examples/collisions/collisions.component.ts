c47f64e6b250e8a67c2ef6da5fcb251af2335da8
Adrian Moore <adrianrob@hotmail.com>
2022-09-15 16:50:29 -0700

    Added pop sounds to collision example

--- src/examples/collisions/collisions.component.ts
+++ src/examples/collisions/collisions.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
-import { Mesh } from "three";
-import { NgtTriple } from "@angular-three/core";
+import { Mesh, AudioListener, PositionalAudio, AudioLoader } from "three";
+import { NgtStore, NgtTriple } from "@angular-three/core";
 
 import { CollisionGroup } from "./collision";
 
@@ -22,7 +22,29 @@ export class CollisionsExample {
   volume = 1;
   get randomInVolume() { return -this.volume + Math.random() * this.volume * 2; }
 
-  constructor() {
+  spheresound!: PositionalAudio;
+  boxsound!: PositionalAudio;
+
+  constructor(private store: NgtStore) {
+    const camera = this.store.get(s => s.camera);
+    const listener = new AudioListener();
+    camera.add(listener);
+
+    // create the PositionalAudio object (passing in the listener)
+    this.spheresound = new PositionalAudio(listener);
+    this.boxsound = new PositionalAudio(listener);
+
+    // load a sound and set it as the PositionalAudio object's buffer
+    const audioLoader = new AudioLoader();
+    audioLoader.load('assets/sphere-pop.wav', (buffer) => {
+      this.spheresound.setBuffer(buffer);
+      this.spheresound.setRefDistance(1);
+    });
+    audioLoader.load('assets/box-pop.wav', (buffer) => {
+      this.boxsound.setBuffer(buffer);
+      this.boxsound.setRefDistance(1);
+    });
+
     const count = 50;
 
     for (let i = 0; i < count; i++) {
@@ -37,22 +59,27 @@ export class CollisionsExample {
         Math.random() * 90
       ));
     }
+
   }
 
   addSphere(mesh: Mesh) {
     this.spherecolliders.addCollider(mesh, 'sphere');
+    mesh.add(this.spheresound);
   }
 
   addBox(mesh: Mesh) {
     this.boxcolliders.addCollider(mesh);
+    mesh.add(this.boxsound);
   }
 
   popSphere(mesh: Mesh) {
+    this.spheresound.play();
     this.spherecolliders.removeCollider(mesh);
     mesh.parent?.remove(mesh);
   }
 
   popBox(mesh: Mesh) {
+    this.boxsound.play();
     this.spherecolliders.removeCollider(mesh);
     mesh.parent?.remove(mesh);
   }
