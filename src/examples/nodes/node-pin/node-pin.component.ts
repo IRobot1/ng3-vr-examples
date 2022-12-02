22c3d4f265b87a49009444bdf51fdc494a72e838
Adrian Moore <adrianrob@hotmail.com>
2022-12-01 18:51:13 -0800

    Draggable pin

--- src/examples/nodes/node-pin/node-pin.component.ts
+++ src/examples/nodes/node-pin/node-pin.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 
-import { Group } from "three";
-import { NgtObjectProps } from "@angular-three/core";
+import { Group, Intersection, Vector3 } from "three";
+import { NgtEvent, NgtObjectProps } from "@angular-three/core";
 
 import { NodeType } from "../node-type/node-type.component";
+
+import { DRAG_END_EVENT, DRAG_MOVE_EVENT, DRAG_START_EVENT, InteractiveObjects } from "ng3-flat-ui";
 
 
 
@@ -31,11 +33,83 @@ export class FlatUINodePin extends NgtObjectProps<Group> implements NodePin {
   @Input() type!: NodeType;
   @Input() value?: any;
   @Input() link = '';
-    object?: Group;
+
+  @Input() selectable?: InteractiveObjects;
+
+  object!: Group;
 
   //@Output() change = new EventEmitter<NodePin>();
 
-  pinready(object: Group) {
-    this.ready.next(object);
+  //
+  // dragging
+  //
+  private isover = false;
+  over() {
+    if (this.isover) return;
+
+    //this.line.visible = true;
+    this.isover = true;
   }
+  out() {
+    //this.line.visible = false;
+    this.isover = false;
+  }
+
+  protected startdragging() {
+    this.dragging = true;
+    this.over();
+    this.object.dispatchEvent({ type: DRAG_START_EVENT })
+  }
+
+  protected enddragging() {
+    this.dragging = false;
+    this.out();
+    this.object.dispatchEvent({ type: DRAG_END_EVENT })
+  }
+
+  protected pinready(pin: Group) {
+    this.selectable?.add(pin);
+
+    pin.addEventListener('pointerdown', (e: any) => {
+      this.startdragging();
+    });
+
+    const dragend = (e: any) => {
+      this.enddragging();
+      e.stop = true;
+    };
+
+    pin.addEventListener('pointerup', dragend);
+    pin.addEventListener('pointerout', dragend);
+    pin.addEventListener('raymissed', dragend);
+
+    pin.addEventListener('pointermove', (e: any) => { this.over(); e.stop = true; });
+
+    this.object = pin;
+    this.ready.next(pin);
+  }
+
+  protected dragging = false;
+
+  protected movepin(event: NgtEvent<PointerEvent>) {
+    event.stopPropagation();
+
+    this.domovepin(event);
+  }
+
+  private domovepin(event: Intersection) {
+
+    if (this.dragging) {
+
+      const position = new Vector3();
+      this.object.getWorldPosition(position);
+
+      this.object.position.x += event.point.x - position.x;
+      this.object.position.y += event.point.y - position.y;
+
+      this.object.dispatchEvent({ type: DRAG_MOVE_EVENT })
+    }
+  }
+
+
 }
